d61763bec5c1f39e64ed9d4b23b0e335233a5b34
Kieran <kierantmcaleer@gmail.com>
2021-04-12 04:46:09 -0400

    finished the website and it is able to scrape

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,22 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 
 import scrape_mars
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
 
 @app.route("/")
 def index():
-    mars_db_data = mongo.db.mars_db.find_one()
-    return render_template("index.html", mars_db_data= mars_db_data)
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
 
 @app.route('/scrape')
 def scrape():
+    mars = mongo.db.mars
     scrape_mars_data = scrape_mars.scrape1()
-    mars_db = mongo.db.mars
-    mars_db.update({}, scrape_mars_data, upsert=True)
+    mars.update({}, scrape_mars_data, upsert=True)
     return redirect('/', code=302)
 
 if __name__ == '__main__':
